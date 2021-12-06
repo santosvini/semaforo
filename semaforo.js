432de8addff2fe60bd97b7e39089041472bd85d6
Vini Santos <vini_santos7@live.com>
2021-12-06 00:55:09 -0300

    :tada: Initial commit, create a trafficLight with VanillaJS :tada:

---
+++ semaforo.js
@@ -0,0 +1,20 @@
+const img = document.getElementById('img')
+const colors = ['red', 'yellow', 'green']
+
+function change() {
+  src = "./img/vermelho.png";
+  img.addEventListener('click', colors.red)
+  console.log('red')
+}
+
+function change() {
+  src = "./img/amarelo.png"
+  img.addEventListener('click', colors.yellow)
+  console.log('yellow')
+}
+
+function change() {
+  src = "./img/verde.png"
+  img.addEventListener('click', colors.green)
+  console.log('green')
+}
